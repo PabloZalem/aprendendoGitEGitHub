ec456dc5a05a892317351b6a5090183491063b84
PabloZalem <69645032+PabloZalem@users.noreply.github.com>
2022-06-14 10:49:31 -0300

    Detalhando como desenvolver como trabalhar com o GitHub para versionar projetos.

--- teste.java
+++ teste.java
@@ -1,5 +1,6 @@
 public class teste{
     public static void main (String[] args){
             System.out.println("Testando");
+            System.out.println("Aprendendo a manipular o GitHub")
     }
 }
