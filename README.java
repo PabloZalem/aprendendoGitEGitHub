a98de5c090f7ebbbb8dee8fa6c7f8aba97730694
PabloZalem <zalem.pablo@yahoo.com>
2022-06-14 19:16:08 -0300

    ATUALIZANDO O README

--- README.java
+++ README.java
@@ -2,4 +2,9 @@ public class README{
     public static void main(String[] args) {
         System.out.println("Esse README foi alterado via IDE");
     }
+
+    /*
+    *   Esse programa tem como finalidade de criar e desenvolver mecanismo de
+    *   manipulação de versionamento de código via git HUB
+    * */
 }
